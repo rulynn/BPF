0be3bb51f8b0d6b3fc7e9adf2887c8641eb3c9a4
linouya <linouyacn@gmail.com>
2020-04-21 21:31:44 +0100

    feat: sync_perf_output

--- learn/[8] sync_perf_output.py
+++ learn/[8] sync_perf_output.py
@@ -17,9 +17,18 @@ b = BPF(text="""
 #include <uapi/linux/ptrace.h>
 BPF_HASH(last);
 
+// define output data structure in C
+struct data_t {
+    u32 pid;
+    u64 ts;
+    u64 delta;
+};
+    
 BPF_PERF_OUTPUT(events);
 
 int do_trace(struct pt_regs *ctx) {
+    struct data_t data = {};
+    
     u64 ts, *tsp, delta, key = 0;
     // attempt to read stored timestamp
     tsp = last.lookup(&key);
@@ -27,7 +36,11 @@ int do_trace(struct pt_regs *ctx) {
         delta = bpf_ktime_get_ns() - *tsp;
         if (delta < 1000000000) {
             // output if time is less than 1 second
-            bpf_trace_printk("%d\\n", delta / 1000000);
+            // bpf_trace_printk("%d\\n", delta / 1000000);
+            data.pid = bpf_get_current_pid_tgid();
+            data.ts = bpf_ktime_get_ns();
+            data.delta = delta / 1000000;
+            events.perf_submit(ctx, &data, sizeof(data));
         }
         last.delete(&key);
     }
@@ -41,14 +54,21 @@ int do_trace(struct pt_regs *ctx) {
 b.attach_kprobe(event=b.get_syscall_fnname("sync"), fn_name="do_trace")
 print("Tracing for quick sync's... Ctrl-C to end")
 
-# format output
+# process event
 start = 0
+def print_event(cpu, data, size):
+    global start
+    event = b["events"].event(data)
+    if start == 0:
+            start = event.ts
+    ts = (float(event.ts - start)) / 1000000000
+    ms = event.delta
+    printb(b"At time %.2f s: multiple syncs detected, last %s ms ago" % (ts, ms))
+
+b["events"].open_perf_buffer(print_event)
+# format outputs
 while 1:
     try:
-        (task, pid, cpu, flags, ts, ms) = b.trace_fields()
-        if start == 0:
-            start = ts
-        ts = ts - start
-        printb(b"At time %.2f s: multiple syncs detected, last %s ms ago" % (ts, ms))
+        b.perf_buffer_poll()
     except KeyboardInterrupt:
         exit()
