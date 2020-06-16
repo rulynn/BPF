8d16fd00d6c4019ff3c70ca9a6c61946adaa4c75
linouya <linouyacn@gmail.com>
2020-06-17 00:26:11 +0800

    update

--- criticality_stacks/dataprocess.py
+++ criticality_stacks/dataprocess.py
@@ -51,7 +51,7 @@ def critical_calculation(locks):
         TIME_MAX = max(TIME_MAX, tmp.lock_time)
 
     # plot critical stack
-    print("max time %d ::: min time %d" % (TIME_MAX, TIME_MIN))
+
     critical_calculation_inner_plot(output_data)
     critical_calculation_inner(output_data)
 
@@ -114,7 +114,7 @@ def critical_calculation_inner(output_data):
     tid_id = 0
     global TIME_MIN
     global TIME_MAX
-
+    print("max time %d ::: min time %d" % (TIME_MAX, TIME_MIN))
     for k, v in output_data.items():
         print("========= pid %d =========" % (k))
         for item in v:
