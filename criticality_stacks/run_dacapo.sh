28e330d07e62049b9bb24f28a164fd3975f51b28
linouya <linouyacn@gmail.com>
2020-07-01 22:44:36 +0800

    update

--- criticality_stacks/run_dacapo.sh
+++ criticality_stacks/run_dacapo.sh
@@ -13,6 +13,10 @@ java -jar dacapo.jar -n 5 avrora &
 
 pid=$(pgrep -f "avrora")
 cd /root/bcc/learn/Master-Project/criticality_stacks
-chmod 777 locktime.py
 
-./locktime.py $pid $time > out.log
+chmod 777 locktime.py
+./locktime.py $pid $time > out.log &
+
+chmod 777 lockstat.py
+./locktime.py $pid > out_stack.log &
+
