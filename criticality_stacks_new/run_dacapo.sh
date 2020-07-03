70907236b6637bae926657ec21b5adaf65e0e0b8
linouya <linouyacn@gmail.com>
2020-07-03 10:42:48 +0800

    fix

---
+++ criticality_stacks_new/run_dacapo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# sh run_dacapo.sh 5
+
+# dir
+rm -rf out
+mkdir out
+time=$1
+
+java -jar ~/dacapo.jar -n 2 avrora &
+pid=$(pgrep -f "avrora")
+
+
+chmod 777 locktime.py
+./locktime.py $pid $time > out.log
+
+#chmod 777 lockstat.py
+#output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid`
+#./lockstat.py $pid > out_stack.log &
+
+
+
