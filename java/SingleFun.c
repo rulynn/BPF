afbb3ff5db00d9557c968dffddd600e59b9f9e2e
linouya <linouyacn@gmail.com>
2020-07-07 16:55:28 +0800

    update

--- java/SingleFun.c
+++ java/SingleFun.c
@@ -5,13 +5,17 @@ using namespace std;
 
 
 int main() {
-    vector<int> nums;
-    for (int i = 0; i < 5000; i++) {
-        nums.push_back(i);
-    }
-	for (int i = 0; i < 5000; i++) {
-        for (int j = 0; j <nums.size() - 1; j++) {
-            nums[j] = nums[j] % nums[j+1] + 1;
+    int times = 5;
+    while(times > 0) {}
+        times--;
+        vector<int> nums;
+        for (int i = 0; i < 100000; i++) {
+            nums.push_back(i);
+        }
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j <nums.size() - 1; j++) {
+                nums[j] = nums[j] % nums[j+1] + 1;
+            }
         }
     }
     cout << "finish" << endl;
