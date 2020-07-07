5c4e052f0990a49ee88f9c3da2fdc94515c87a8f
linouya <linouyacn@gmail.com>
2020-07-07 17:11:34 +0800

    update

--- java/SingleFun.c
+++ java/SingleFun.c
@@ -9,10 +9,10 @@ int main() {
     while(times > 0) {
         times--;
         vector<int> nums;
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 10000; i++) {
             nums.push_back(i);
         }
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 10000; i++) {
             for (int j = 0; j <nums.size() - 1; j++) {
                 nums[j] = nums[j] % nums[j+1] + 1;
             }
