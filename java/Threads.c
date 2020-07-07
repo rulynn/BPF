83ffcf26723168966725afc860ce85a8f0e51f2c
linouya <linouyacn@gmail.com>
2020-07-07 17:53:30 +0800

    update

---
+++ java/Threads.c
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <thread>
+
+#include <mutex>
+
+using namespace std;
+
+
+volatile int val;
+
+mutex mut;
+
+
+void icrement () {
+
+    for (int i = 0; i < 100000000; i++) {
+        mut.lock ();
+        val++;
+        mut.unlock ();
+    }
+}
+
+
+int main (int argc, char* argv []) {
+
+    int times = 5;
+    while(times > 0) {
+        times--;
+        // Two threads
+        thread t1 (icrement);
+        thread t2 (icrement);
+
+        // wait
+        t1.join ();
+        t2.join ();
+        cout << val << endl;
+        return 0;
+    }
+}
