5f8b26730a300503d9c99cd61229fe4281799836
linouya <linouyacn@gmail.com>
2020-07-08 17:25:54 +0800

    update

---
+++ java/Threads.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @Author : linouya
+ * @Date : Created in 5:25 下午 2020/7/8
+ */
+public class Threads {
+
+    public static void main(String[] args) throws IOException {
+        TestThread testThread = new TestThread();
+        Thread thread1 = new Thread(testThread);
+        Thread thread2 = new Thread(testThread);
+        thread1.start();
+        thread2.start();
+    }
+
+
+}
+
+class TestThread implements Runnable{
+    private Long val = 0L;
+
+    @Override
+    public synchronized void run() {
+        for (int i = 0; i < 400000000; i++) {
+            val++;
+        }
+        System.out.println(val);
+    }
+}
