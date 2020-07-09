9af74bfeb47a574744cdf05f9cfc62831e410a08
linouya <linouyacn@gmail.com>
2020-07-09 15:13:53 +0800

    update

--- criticality_stacks/locktime.py
+++ criticality_stacks/locktime.py
@@ -6,7 +6,7 @@ from time import sleep
 from bcc import BPF
 import process
 import stack
-
+import itertools
 
 if len(sys.argv) < 3:
     print("USAGE: need PID and time")
@@ -25,6 +25,40 @@ bpf.attach_uprobe(name="pthread", sym="pthread_mutex_unlock", fn_name="probe_mut
 locks = bpf["locks"]
 init_stacks = bpf["init_stacks"]
 stacks = bpf["stacks"]
+
 sleep(int(time))
 process.main(locks)
-stack.main(bpf, pid, locks, init_stacks, stacks)
+main()
+# stack.main(bpf, pid, locks, init_stacks, stacks)
+
+
+
+def print_frame(bpf, pid, addr):
+    print("\t\t%16s (%x)" % (bpf.sym(addr, pid, show_module=True, show_offset=True), addr))
+
+def print_stack(bpf, pid, stacks, stack_id):
+    for addr in stacks.walk(stack_id):
+        print_frame(bpf, pid, addr)
+
+def main():
+    print("................... stack start ...................")
+    mutex_ids = {}
+    next_mutex_id = 1
+    for k, v in init_stacks.items():
+        mutex_id = "#%d" % next_mutex_id
+        next_mutex_id += 1
+        mutex_ids[k.value] = mutex_id
+        print("init stack for mutex %x (%s)" % (k.value, mutex_id))
+        print_stack(bpf, pid, stacks, v.value)
+        print("")
+    grouper = lambda (k, v): k.tid
+    sorted_by_thread = sorted(locks.items(), key=grouper)
+    locks_by_thread = itertools.groupby(sorted_by_thread, grouper)
+    for tid, items in locks_by_thread:
+        print("thread %d" % tid)
+        for k, v in sorted(items, key=lambda (k, v): -v.wait_time_ns):
+            #mutex_descr = mutex_ids[k.mtx] if k.mtx in mutex_ids else bpf.sym(k.mtx, pid)
+            print("\tmtx %s ::: wait time %.2fus ::: hold time %.2fus ::: enter count %d" %
+                  (k.mtx, v.wait_time_ns/1000.0, v.lock_time_ns/1000.0, v.enter_count))
+            print_stack(bpf, pid, stacks, k.lock_stack_id)
+            print("")
