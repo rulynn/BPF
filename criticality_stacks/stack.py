0104aaa9af62d3c1ca2f526fef3eab037904115d
linouya <linouyacn@gmail.com>
2020-07-09 14:38:19 +0800

    update

--- criticality_stacks/stack.py
+++ criticality_stacks/stack.py
@@ -9,7 +9,7 @@ def print_stack(bpf, pid, stacks, stack_id):
         print_frame(bpf, pid, addr)
 
 def main(init_stacks, stacks):
-    print(""................... stack start ...................")
+    print("................... stack start ...................")
     mutex_ids = {}
     next_mutex_id = 1
     for k, v in init_stacks.items():
