5d2a2fc90f3beb7ae1f1d0485405d23b9d362ea0
linouya <linouyacn@gmail.com>
2020-07-09 16:43:21 +0800

    update

--- criticality_stacks/locktime.py
+++ criticality_stacks/locktime.py
@@ -17,7 +17,7 @@ debug = 0
 
 # load BPF program
 bpf = BPF(src_file = "locktime.c")
-bpf.attach_uprobe(name="pthread", sym="pthread_mutex_init", fn_name="probe_mutex_init", pid=int(pid))
+#bpf.attach_uprobe(name="pthread", sym="pthread_mutex_init", fn_name="probe_mutex_init", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock", pid=int(pid))
 bpf.attach_uretprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock_return", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_unlock", fn_name="probe_mutex_unlock", pid=int(pid))
