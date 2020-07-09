118add8c6fad98912451e47a61bdf12ded4d8de3
linouya <linouyacn@gmail.com>
2020-07-09 17:21:19 +0800

    update

--- criticality_stacks_old/run_stat.sh
+++ criticality_stacks_old/run_stat.sh
@@ -5,22 +5,21 @@
 
 name="SingleFun"
 
-echo "start running program..."
 cd ../java
-java -XX:+ExtendedDTraceProbes $name &
+javac Threads.java
+java -XX:+ExtendedDTraceProbes -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints Threads &
+sleep 1
 
-echo "start get pid..."
+
 pid=$(pgrep -f "$name")
 echo "program pid: "  $pid
 
-echo "start perf map"
+
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid`
 
-echo "start running eBPF"
-cd ../learn
-chmod 777 lockstat.py
-./lockstat.py $pid > out.log
 
-echo "start kill the program..."
-kill -9 $pid
-echo "finish"
+cd ../criticality_stacks_old
+chmod 777 lockstat.py
+./lockstat.py $pid > out_stack.log
+
+echo "eBPF finish"
