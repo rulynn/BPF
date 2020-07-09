88026968bb5369a2fc9b39e13eef577e95e88bba
linouya <linouyacn@gmail.com>
2020-07-09 13:08:52 +0800

    update

--- criticality_stacks/run_splash.sh
+++ criticality_stacks/run_splash.sh
@@ -7,19 +7,17 @@ rm -rf out
 mkdir out
 time=$1
 
-echo "start running program"
-#~/splash2/codes/kernels/fft/FFT -p8 -m26 &
-
-~/splash2/codes/kernels/lu/non_contiguous_blocks/LU -p8 -n4096 &
+~/splash2/codes/kernels/fft/FFT -p8 -m26 &
+pid=$(pgrep -f "FFT")
 
 #~/splash2/codes/kernels/cholesky/CHOLESKY -p8 < inputs/tk29.O &
+#pid=$(pgrep -f "CHOLESKY")
 
-#cd /root/splash2/codes/apps/ocean/contiguous_partitions
-#./OCEAN -p8 -n2050 &
+#~/splash2/codes/kernels/lu/non_contiguous_blocks/LU -p8 -n4096 &
+#pid=$(pgrep -f "LU")
 
-pid=$(pgrep -f "LU")
+
 echo "program pid: "  $pid
-
 chmod 777 locktime.py
 ./locktime.py $pid $time > out.log
 
