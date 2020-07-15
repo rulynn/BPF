27798fe779e19e22071f363eb17a8b0ece96c02c
linouya <linouyacn@gmail.com>
2020-07-15 21:21:41 +0800

    update

--- criticality_stacks/run_dacapo.sh
+++ criticality_stacks/run_dacapo.sh
@@ -7,14 +7,14 @@ rm -rf out
 mkdir out
 time=$1
 
-java -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 5 avrora &
-sleep 1
+java -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 1 avrora &
+#sleep 1
 
 pid=$(pgrep -f "avrora")
 echo "program pid: "  $pid
 
 # jstack
-jstack $pid
+jstack $pid > out_stack.log &
 
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
 
