71a1a969f1cc36f6f9d48cae05cffd7144635640
linouya <linouyacn@gmail.com>
2020-07-16 15:03:33 +0800

    update

--- criticality_stacks/process.py
+++ criticality_stacks/process.py
@@ -173,9 +173,16 @@ def plot():
     pre.append(0)
     pre.append(0)
     pre.append(0)
+    sub_tid = []
+    sub_total = 0
     for i in range(len(tid_list)):
         if ans[i] == 0:
             continue
+
+        if ans[i]/total < 0.2:
+            sub_tid.append(i)
+            sub_total += ans[i]
+
         print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/total))
         label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i], 0)) + "/" + str(round(total,0)) + "=" + str(round(ans[i]/total,4))
         width = 0.35
@@ -194,7 +201,31 @@ def plot():
     plt.legend()
     path = "out/critical.png"
     plt.savefig(path)
+    subplot()
 
+def subplot(sub_tid, sub_total):
+
+    global tid_list
+    global ans
+
+    for i in sub_tid:
+        label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i], 0)) + "/" + str(round(sub_total,0)) + "=" + str(round(ans[i]/sub_total,4))
+        width = 0.35
+
+        now = []
+        now.append(pre[0] + ans[i]/sub_total)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
+
+        pre = now
+
+    plt.ylim(0,1)
+    plt.legend()
+    path = "out/critical-sub.png"
+    plt.savefig(path)
 
 
 # delete: memory error
