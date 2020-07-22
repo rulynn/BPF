4d7856a935317bca09b4fd9a3d45ffcea5b7604e
linouya <linouyacn@gmail.com>
2020-07-22 21:08:39 +0800

    update

--- criticality_stacks/run_profile.sh
+++ criticality_stacks/run_profile.sh
@@ -4,13 +4,17 @@
 rm -rf out
 mkdir out
 time=$1
-name="avrora"
+name="SingleFun"
 
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ~/dacapo.jar -n 3 $name &
+cd ../java
+javac $name.java
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints $name &
 
 pid=$(pgrep -f "$name")
 echo "program pid: "  $pid
 
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid`
+
+cd ../criticality_stacks
 output=`~/bcc/tools/profile.py -adf -p $pid $time > out/out.profile`
 output=`~/FlameGraph/flamegraph.pl < out/out.profile > out/out.svg`
