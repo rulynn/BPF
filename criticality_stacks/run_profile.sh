864ac6317d5760ce4c5a4c3be809661112c29c99
linouya <linouyacn@gmail.com>
2020-07-22 18:02:16 +0800

    update

--- criticality_stacks/run_profile.sh
+++ criticality_stacks/run_profile.sh
@@ -11,16 +11,11 @@ echo "start running program..."
 cd ../java
 java -XX:+ExtendedDTraceProbes $name &
 
-echo "start get pid..."
 pid=$(pgrep -f "$name")
 echo "program pid: "  $pid
 
-echo "start perf map"
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid`
 
-echo "start profile"
 cd ../learn/tools
 output=`./profile.py -adf -p $pid $time > out.profile`
-
-echo "start flamegraph"
 output=`~/FlameGraph/flamegraph.pl < out.profile > out.svg`
