f6732075ef297c2a0f5efc1a383c72cc84554062
linouya <linouyacn@gmail.com>
2020-07-24 10:11:16 +0800

    update

--- demo/run_dacapo.sh
+++ demo/run_dacapo.sh
@@ -15,13 +15,14 @@ pid=$(pgrep -f "avrora")
 echo "program pid: "  $pid
 
 # jstack
-jstack $pid > out_stack.log &
+jstack $pid > out/out_stack.log &
 
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
 chmod 777 locktime.py
-./locktime.py $pid $time > out.log &
+./locktime.py $pid $time > out/out.log &
 
 # flamegraph
+cd out
 output=`perf record -F 99 -p $pid -g -- sleep $time`
 perf script -i perf.data &> perf.unfold
 ~/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
