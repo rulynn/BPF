eda369e9fbf4f2bca3b194e0d20b874bd7283f97
linouya <linouyacn@gmail.com>
2020-07-24 12:19:50 +0800

    update

--- bash/run_dacapo.sh
+++ bash/run_dacapo.sh
@@ -10,8 +10,9 @@ file_path="../criticality_stacks"
 # out path
 rm -rf $out_path
 mkdir $out_path
+cd $out_path
 
-# -s large -n 5 -Xmx1024m
+# Dacapo -s large -n 5 -Xmx1024m
 java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 2 $name &
 sleep 1
 
@@ -19,12 +20,12 @@ pid=$(pgrep -f "$name")
 echo "program pid: "  $pid
 
 # jstack
-output=`jstack $pid > $out_path/out_stack.log`
+output=`jstack $pid > out_stack.log`
 # perf map
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
 # eBPF
 chmod 777 $file_path/locktime.py
-$file_path/locktime.py $pid $time > $out_path/out.log &
+$file_path/locktime.py $pid $time > out.log &
 
 #flamegraph
 sh run_flamegraph.sh $time $pid
