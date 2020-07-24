e7de77a4b31e65f8935c1ed1da28166d175c2217
linouya <linouyacn@gmail.com>
2020-07-24 10:05:59 +0800

    update

--- demo/locktime.py
+++ demo/locktime.py
@@ -13,15 +13,10 @@ if len(sys.argv) < 3:
     exit()
 pid = sys.argv[1]
 time = sys.argv[2]
-isStack = True
 
 # load BPF program
-
-if isStack == True:
-    bpf = BPF(src_file = "locktime_stack.c")
-    bpf.attach_uprobe(name="pthread", sym="pthread_mutex_init", fn_name="probe_mutex_init", pid=int(pid))
-else:
-    bpf = BPF(src_file = "locktime.c")
+bpf = BPF(src_file = "locktime.c")
+bpf.attach_uprobe(name="pthread", sym="pthread_mutex_init", fn_name="probe_mutex_init", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock", pid=int(pid))
 bpf.attach_uretprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock_return", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_unlock", fn_name="probe_mutex_unlock", pid=int(pid))
@@ -30,7 +25,6 @@ locks = bpf["locks"]
 
 sleep(int(time))
 process.run(locks)
-if isStack == True:
-    init_stacks = bpf["init_stacks"]
-    stacks = bpf["stacks"]
-    stack.run(bpf, int(pid), locks, init_stacks, stacks)
+init_stacks = bpf["init_stacks"]
+stacks = bpf["stacks"]
+stack.run(bpf, int(pid), locks, init_stacks, stacks)
