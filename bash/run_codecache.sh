0a190b9cca6f50c51a77bbf1c4c9849742ee4abc
linouya <linouyacn@gmail.com>
2020-07-31 10:53:09 +0800

    update

--- bash/run_codecache.sh
+++ bash/run_codecache.sh
@@ -15,19 +15,16 @@ cd $out_path
 
 # Dacapo -s large -n 5 -Xmx1024m
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
-output=`java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:ReservedCodeCacheSize=512M -jar ~/dacapo.jar -n 2 $name`
-endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date="$starttime" +%s);
+start_timeStamp=$((start_seconds*1000+`date "+%N"`/1000000))
+
+output=`java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:ReservedCodeCacheSize=512M -jar ~/dacapo.jar -n 2 $name`
+
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
 end_seconds=$(date --date="$endtime" +%s);
-echo "runtime:"$((end_seconds-start_seconds))"s"
+end_timeStamp=$((end_seconds*1000+`date "+%N"`/1000000))
+echo "runtime:"$((start_timeStamp-end_timeStamp))"s"
 
-
-#flamegraph
-#output=`perf record -F 99 -p $pid -g -- sleep $time`
-#
-#perf script -i perf.data &> perf.unfold
-#~/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
-#~/FlameGraph/flamegraph.pl perf.folded > perf.svg
 
 
 
