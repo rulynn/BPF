9b2e79c68ac06214da1ce359cc5495e684928e39
linouya <linouyacn@gmail.com>
2020-07-31 10:47:51 +0800

    update

---
+++ bash/run_codecache.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# sh run_dacapo.sh 30
+
+time=$1
+name="avrora"
+out_path="../out"
+file_path="../criticality_stacks"
+bash_path="../bash"
+
+# out path
+rm -rf $out_path
+mkdir $out_path
+cd $out_path
+
+# Dacapo -s large -n 5 -Xmx1024m
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+begin=$((timeStamp*1000+10#`date "+%N"`/1000000))
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "runtime:"$((end_seconds-start_seconds))"s"
+
+
+#flamegraph
+#output=`perf record -F 99 -p $pid -g -- sleep $time`
+#
+#perf script -i perf.data &> perf.unfold
+#~/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
+#~/FlameGraph/flamegraph.pl perf.folded > perf.svg
+
+
+
+
