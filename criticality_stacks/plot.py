cc7efd14820869ff2ffd4ffd92b517b08f3891ab
linouya <linouyacn@gmail.com>
2020-07-31 12:43:25 +0800

    update

--- criticality_stacks/plot.py
+++ criticality_stacks/plot.py
@@ -8,10 +8,10 @@ import csv
 
 
 def run(tid_list, ans, total):
-#     plot_origin(tid_list, ans, total)
-#     plot_with_name(tid_list, ans, total)
-#     plot_sub(tid_list, ans, total)
-    outputCSV(tid_list, ans, total)
+    plot_origin(tid_list, ans, total)
+    plot_with_name(tid_list, ans, total)
+    plot_sub(tid_list, ans, total)
+    #outputCSV(tid_list, ans, total)
 
 def outputCSV(tid_list, ans, total):
 
@@ -38,120 +38,120 @@ def outputCSV(tid_list, ans, total):
     csvfile.close()
 
 
-#
-#
-# def plot_origin(tid_list, ans, total):
-#     plt.figure(1)
-#     # plot
-#     pre = []
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     sub_tid = []
-#     sub_total = 0
-#     for i in range(len(tid_list)):
-#         if ans[i] == 0:
-#             continue
-#
-#         print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/total))
-#         label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i], 0)) + "/" + str(round(total,0)) + "=" + str(round(ans[i]/total,4))
-#         width = 0.35
-#
-#         now = []
-#         now.append(pre[0] + ans[i]/total)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
-#
-#         pre = now
-#
-#     plt.ylim(0,1)
-#     plt.legend()
-#     path = "../out/critical-origin.png"
-#     plt.savefig(path)
-#
-# def plot_with_name(tid_list, ans, total):
-#     plt.figure(2)
-#     VMThread = getVMThread()
-#     # plot
-#     pre = []
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     sub_tid = []
-#     sub_total = 0
-#     for i in range(len(tid_list)):
-#         if ans[i] == 0:
-#             continue
-#         print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/total))
-#         label = ""
-#         if VMThread.get(tid_list[i]) == None:
-#             label = "thread " + str(tid_list[i]);
-#         else:
-#             label = VMThread[tid_list[i]] + " " + str(tid_list[i])
-#         label = label + ": " + str(round(ans[i]/total,4))
-#         width = 0.35
-#
-#         now = []
-#         now.append(pre[0] + ans[i]/total)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
-#
-#         pre = now
-#
-#     plt.ylim(0,1)
-#     plt.legend()
-#     path = "../out/critical-name.png"
-#     plt.savefig(path)
-#
-# def plot_sub(tid_list, ans, total):
-#     plt.figure(3)
-#     VMThread = getVMThread()
-#     # plot
-#     pre = []
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     pre.append(0)
-#     sub_tid = []
-#     sub_total = 0
-#     for i in range(len(tid_list)):
-#         if ans[i] == 0:
-#                 continue
-#         if VMThread.get(tid_list[i]) == None:
-#             sub_tid.append(i)
-#             sub_total += ans[i]
-#
-#     for i in sub_tid:
-#         print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/sub_total))
-#         label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i]/sub_total,4))
-#         width = 0.35
-#
-#         now = []
-#         now.append(pre[0] + ans[i]/sub_total)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         now.append(0)
-#         plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
-#
-#         pre = now
-#
-#     plt.ylim(0,1)
-#     plt.legend()
-#     path = "../out/critical-sub.png"
-#     plt.savefig(path)
-#
+
+
+def plot_origin(tid_list, ans, total):
+    plt.figure(1)
+    # plot
+    pre = []
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    sub_tid = []
+    sub_total = 0
+    for i in range(len(tid_list)):
+        if ans[i] == 0:
+            continue
+
+        print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/total))
+        label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i], 0)) + "/" + str(round(total,0)) + "=" + str(round(ans[i]/total,4))
+        width = 0.35
+
+        now = []
+        now.append(pre[0] + ans[i]/total)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
+
+        pre = now
+
+    plt.ylim(0,1)
+    plt.legend()
+    path = "../out/critical-origin.png"
+    plt.savefig(path)
+
+def plot_with_name(tid_list, ans, total):
+    plt.figure(2)
+    VMThread = getVMThread()
+    # plot
+    pre = []
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    sub_tid = []
+    sub_total = 0
+    for i in range(len(tid_list)):
+        if ans[i] == 0:
+            continue
+        print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/total))
+        label = ""
+        if VMThread.get(tid_list[i]) == None:
+            label = "thread " + str(tid_list[i]);
+        else:
+            label = VMThread[tid_list[i]] + " " + str(tid_list[i])
+        label = label + ": " + str(round(ans[i]/total,4))
+        width = 0.35
+
+        now = []
+        now.append(pre[0] + ans[i]/total)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
+
+        pre = now
+
+    plt.ylim(0,1)
+    plt.legend()
+    path = "../out/critical-name.png"
+    plt.savefig(path)
+
+def plot_sub(tid_list, ans, total):
+    plt.figure(3)
+    VMThread = getVMThread()
+    # plot
+    pre = []
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    pre.append(0)
+    sub_tid = []
+    sub_total = 0
+    for i in range(len(tid_list)):
+        if ans[i] == 0:
+                continue
+        if VMThread.get(tid_list[i]) == None:
+            sub_tid.append(i)
+            sub_total += ans[i]
+
+    for i in sub_tid:
+        print("ans %d ::: total %d ::: ans/total %.2f" % (ans[i], total, ans[i]/sub_total))
+        label = "thread " + str(tid_list[i]) + ": " + str(round(ans[i]/sub_total,4))
+        width = 0.35
+
+        now = []
+        now.append(pre[0] + ans[i]/sub_total)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        now.append(0)
+        plt.bar((1,2,3,4,5), now, width, bottom=pre, label=label)
+
+        pre = now
+
+    plt.ylim(0,1)
+    plt.legend()
+    path = "../out/critical-sub.png"
+    plt.savefig(path)
+
 
 def getVMThread():
     VMThread = {}
