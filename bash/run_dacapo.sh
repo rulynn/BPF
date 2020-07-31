32c7e5adf04f9cf7920aaa73ff3ee475c14775a7
linouya <linouyacn@gmail.com>
2020-07-31 13:29:37 +0800

    update

--- bash/run_dacapo.sh
+++ bash/run_dacapo.sh
@@ -14,7 +14,7 @@ mkdir $out_path
 cd $out_path
 
 # Dacapo -s large -n 5 -Xmx1024m -XX:ReservedCodeCacheSize=64M
-java -XX:ReservedCodeCacheSize=64M -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ~/dacapo.jar -n 5 $name &
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ~/dacapo.jar -n 2 $name &
 #sleep 1
 
 pid=$(pgrep -f "$name")
