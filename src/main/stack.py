b0f0a2786b58ebddb0805d1dfe3fd80d185ca9a4
linouya <linouyacn@gmail.com>
2020-08-04 20:46:01 +0800

    update: output to file

--- src/main/stack.py
+++ src/main/stack.py
@@ -70,6 +70,7 @@ def run2(bpf, pid, locks):
         line = [k.name]
         line.extend([bpf.sym(addr, k.pid) for addr in reversed(user_stack)])
         print(k.tid)
+        print(k.pid)
         file = str(k.tid) + ".json"
         with open(file, 'a', encoding="utf-8") as f:
             s = ";".join(line).decode('utf-8', 'replace') + " " + v.value
