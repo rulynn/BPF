318ebddb0b25c41910e99465e2c564065c07f7e4
linouya <linouyacn@gmail.com>
2020-08-05 23:34:20 +0800

    update

--- test/locktime_test.py
+++ test/locktime_test.py
@@ -14,16 +14,30 @@ if len(sys.argv) < 3:
 pid = sys.argv[1]
 time = sys.argv[2]
 isStack = True
-languages = "java"
-
 usdt = USDT(pid=int(pid))
 usdt.enable_probe_or_bail("pthread_start", "trace_pthread")
+
+language = "java"
+
 if language == "c":
+    # Nothing to add
     pass
 elif language == "java":
     usdt.enable_probe_or_bail("thread__start", "trace_start")
     usdt.enable_probe_or_bail("thread__stop", "trace_stop")
 
+bpf = BPF(src_file = "test.c", usdt_contexts=[usdt])
+print("Tracing thread events in process %d (language: %s)... Ctrl-C to quit." %
+      (int(pid), language or "none"))
+print("%-8s %-16s %-8s %-30s" % ("TIME", "ID", "TYPE", "DESCRIPTION"))
+
+class ThreadEvent(ct.Structure):
+    _fields_ = [
+        ("runtime_id", ct.c_ulonglong),
+        ("native_id", ct.c_ulonglong),
+        ("type", ct.c_char * 8),
+        ("name", ct.c_char * 80),
+        ]
 
 # load BPF program
 if isStack == True:
@@ -42,12 +56,16 @@ locks = bpf["locks"]
 sleep(int(time))
 #process.run(locks)
 if isStack == True:
+    start_ts = time.time()
+
     print("start to print threads")
     threads = bpf["threads"]
     print(threads)
     print(len(threads))
     for k, event in threads.items():
-        print(k, event)
+        #event = ct.cast(event, ct.POINTER(ThreadEvent)).contents
+        print(k.runtime_id)
+        #print(event.value)
         name = event.name
         if event.type == "pthread":
             name = bpf.sym(event.runtime_id, args.pid, show_module=True)
@@ -56,7 +74,6 @@ if isStack == True:
             tid = "R=%s/N=%s" % (event.runtime_id, event.native_id)
         print("%-8.3f %-16s %-8s %-30s" % (
             time.time() - start_ts, tid, event.type, name))
-
     #stack.run2(bpf, int(pid), locks)
     #stack.run(bpf, int(pid), locks, init_stacks, stacks)
     #stack.test_stack(bpf)
