99b16e4625706956244265ed1e00a4755708927c
linouya <linouyacn@gmail.com>
2020-08-06 23:35:22 +0800

    update

--- resources/code/ThreadsWithLock.java
+++ resources/code/ThreadsWithLock.java
@@ -11,7 +11,7 @@ public class ThreadsWithLock {
     private static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
-        int times = 1;
+        int times = 5;
         while(times > 0) {
             times--;
             TestThread testThread = new TestThread();
