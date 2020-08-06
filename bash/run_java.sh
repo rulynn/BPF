278ac5674ed8b4de58e30b680680d29f399532dc
linouya <linouyacn@gmail.com>
2020-08-06 23:30:25 +0800

    update

--- bash/run_java.sh
+++ bash/run_java.sh
@@ -13,7 +13,7 @@ mkdir $out_path/stack
 
 # Modify java program: Replace 'SingleFun' in java and java_name
 # java program shows in ../java/
-name="SingleFun" #ThreadsWithLock
+name="ThreadsWithLock" #ThreadsWithLock
 javac $name.java
 java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer $name &
 #sleep 1
