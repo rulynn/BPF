70889e72c0be20e370630648a1e1ca320ebc6dcd
linouya <linouyacn@gmail.com>
2020-08-06 20:57:04 +0800

    update

--- bash/run_dacapo.sh
+++ bash/run_dacapo.sh
@@ -27,11 +27,11 @@ output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedcl
 chmod 777 main/locktime.py
 output=`main/locktime.py $pid $time > output/out.log`
 # burn: convert data to json
-chmod 777 $burn_path/burn
-for file in output/stack/*; do
-    echo $file
-    $burn_path/burn convert --type=folded $file > $file.json
-done
+#chmod 777 $burn_path/burn
+#for file in output/stack/*; do
+#    echo $file
+#    $burn_path/burn convert --type=folded $file > $file.json
+#done
 
 
 
