f723368e9e4121434996c741ffb223ff87264da7
linouya <linouyacn@gmail.com>
2020-08-06 23:24:39 +0800

    update

--- bash/run_java.sh
+++ bash/run_java.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# dir
-rm -rf out
-mkdir out
 time=$1
+burn_path="../"
+out_path="../../src/output"
+main_path="../../src/main"
 
+rm -rf $out_path
+mkdir $out_path
+mkdir $out_path/stack
+cd ../resources/code
 
 # Modify java program: Replace 'SingleFun' in java and java_name
 # java program shows in ../java/
-cd ../java
 name="SingleFun" #ThreadsWithLock
 javac $name.java
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints $name &    #
-sleep 1
-
-#java Threads &
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer $name &
 #sleep 1
 
 pid=$(pgrep -f "$name")
-echo "program pid: "  $pid
+echo "program pid: " $pid
 
 # jstack
-jstack $pid
-
+output=`jstack $pid > $out_path/out_stack.log`
+# perf map
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
+# eBPF
+chmod 777 $main_path/locktime.py
+output=`$main_path/locktime.py $pid $time > $out_path/out.log`
 
-cd ../criticality_stacks
-chmod 777 locktime.py
-./locktime.py $pid $time > out.log
-echo "eBPF finish"
-
+# burn: convert data to json
+chmod 777 $burn_path/burn
+for file in $out_path/stack/*; do
+    echo $file
+    $burn_path/burn convert --type=folded $file > $file.json
+done
