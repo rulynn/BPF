9a802de4090203c5a94dd3db0ef88d4e5c190cdc
linouya <linouyacn@gmail.com>
2020-08-09 12:37:55 +0800

    update

--- bash/run_splash.sh
+++ bash/run_splash.sh
@@ -10,14 +10,14 @@ rm -rf $out_path
 mkdir $out_path
 cd ../src
 
-#~/splash2/codes/kernels/fft/FFT -p8 -m26 &
-#pid=$(pgrep -f "FFT")
+~/splash2/codes/kernels/fft/FFT -p8 -m26 &
+pid=$(pgrep -f "FFT")
 
 #~/splash2/codes/kernels/cholesky/CHOLESKY -p8 < ~/splash2/codes/kernels/cholesky/inputs/tk29.O &
 #pid=$(pgrep -f "CHOLESKY")
 
-~/splash2/codes/kernels/lu/non_contiguous_blocks/LU -p8 -n4096 &
-pid=$(pgrep -f "LU")
+#~/splash2/codes/kernels/lu/non_contiguous_blocks/LU -p8 -n4096 &
+#pid=$(pgrep -f "LU")
 
 echo "program pid: " $pid
 
