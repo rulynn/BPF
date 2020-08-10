9a950d2315075b6a1382524dd378057bb5d2724e
linouya <linouyacn@gmail.com>
2020-08-10 10:52:42 +0800

    update

--- src/code/Threads.java
+++ src/code/Threads.java
@@ -7,7 +7,7 @@ public class Threads {
     public static volatile Long val = 0L;
 
     public static void main(String[] args) throws InterruptedException {
-        int times = 5;
+        int times = 1;
         while(times > 0) {
             times--;
             TestThread testThread = new TestThread();
@@ -27,7 +27,7 @@ public class Threads {
         public void run() {
             synchronized (this) {
                 System.out.println("now tid: " + Thread.currentThread().getId());
-                for (int i = 0; i < 100000000; i++) {
+                for (int i = 0; i < 1000000000; i++) {
                     val++;
                 }
             }
