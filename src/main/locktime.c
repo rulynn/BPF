12afe6d050abe6f810929842140d483f0cd59717
linouya <linouyacn@gmail.com>
2020-08-10 15:46:38 +0800

    update

--- src/main/locktime.c
+++ src/main/locktime.c
@@ -155,3 +155,14 @@ int probe_join(struct pt_regs *ctx){
     test.increment(unit);
     return 0;
 }
+
+int probe_cancel(struct pt_regs *ctx){
+    u64 now = bpf_ktime_get_ns();
+    struct test_unit unit = {};
+    unit.timestamp = now;
+    unit.tid = bpf_get_current_pid_tgid();
+    unit.mtx = PT_REGS_PARM1(ctx);
+    unit.type = 5;
+    test.increment(unit);
+    return 0;
+}
--- src/main/locktime.py
+++ src/main/locktime.py
@@ -30,6 +30,7 @@ bpf.attach_uprobe(name="pthread", sym="pthread_create", fn_name="probe_create", 
 bpf.attach_uprobe(name="pthread", sym="pthread_exit", fn_name="probe_exit", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_trylock", fn_name="probe_mutex_trylock", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_join", fn_name="probe_join", pid=int(pid))
+bpf.attach_uprobe(name="pthread", sym="pthread_cancel", fn_name="probe_cancel", pid=int(pid))
 
 
 
