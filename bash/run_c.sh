b66cbdc038882bb029b346784af46bdbfcbbd853
linouya <linouyacn@gmail.com>
2020-08-10 13:48:15 +0800

    update

--- bash/run_c.sh
+++ bash/run_c.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# dir
-rm -rf out
-mkdir out
 time=$1
+burn_path="../resources"
+out_path="../src/output"
 
+rm -rf $out_path
+mkdir $out_path
+mkdir $out_path/stack
 
-# Modify java program: Replace 'SingleFun' in java and java_name
-# java program shows in ../java/
-
-cd ../
+cd ../src/code
+name="Threads" #ThreadsWithLock
 g++ Threads.c -o Threads -lpthread
 ./Threads &
+cd ..
 
 pid=$(pgrep -f "./Threads")
-echo "program pid: "  $pid
+echo "program pid: " $pid
 
-cd ../criticality_stacks
-chmod 777 locktime.py
-./locktime.py $pid $time > out.log
-echo "eBPF finish"
-
-
-
+# eBPF
+chmod 777 main/locktime.py
+output=`main/locktime.py $pid $time > output/out.log`
 
 
 
