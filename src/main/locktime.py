d49621482605305e10c5ad4ff75e530b171c3721
linouya <linouyacn@gmail.com>
2020-08-17 22:04:04 +0800

    update

--- src/main/locktime.py
+++ src/main/locktime.py
@@ -13,28 +13,25 @@ if len(sys.argv) < 3:
     exit()
 pid = sys.argv[1]
 time = sys.argv[2]
-isStack = False
+isStack = True
 
-# usdt = USDT(pid=int(pid))
-# usdt.enable_probe_or_bail("pthread_create", "trace_pthread")
+usdt = USDT(pid=int(pid))
+usdt.enable_probe_or_bail("pthread_create", "trace_pthread")
 
 # load BPF program
 if isStack == True:
-#     usdt.enable_probe_or_bail("thread__start", "trace_start")
-#     usdt.enable_probe_or_bail("thread__stop", "trace_stop")
-#     bpf = BPF(src_file = "locktime_stack.c", usdt_contexts=[usdt])
-    bpf = BPF(src_file = "locktime_stack.c")
+    usdt.enable_probe_or_bail("thread__start", "trace_start")
+    usdt.enable_probe_or_bail("thread__stop", "trace_stop")
+    bpf = BPF(src_file = "locktime_stack.c", usdt_contexts=[usdt])
     bpf.attach_uprobe(name="pthread", sym="pthread_mutex_init", fn_name="probe_mutex_init", pid=int(pid))
 else:
-    bpf = BPF(src_file = "locktime.c")
-    #bpf = BPF(src_file = "locktime.c", usdt_contexts=[usdt])
+    bpf = BPF(src_file = "locktime.c", usdt_contexts=[usdt])
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock", pid=int(pid))
 bpf.attach_uretprobe(name="pthread", sym="pthread_mutex_lock", fn_name="probe_mutex_lock_return", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_mutex_unlock", fn_name="probe_mutex_unlock", pid=int(pid))
 
 # new
 bpf.attach_uprobe(name="pthread", sym="pthread_create", fn_name="probe_create", pid=int(pid))
-bpf.attach_uprobe(name="pthread", sym="pthread_start", fn_name="trace_pthread", pid=int(pid))
 # bpf.attach_uprobe(name="pthread", sym="pthread_exit", fn_name="probe_exit", pid=int(pid))
 # bpf.attach_uprobe(name="pthread", sym="pthread_mutex_trylock", fn_name="probe_mutex_trylock", pid=int(pid))
 bpf.attach_uprobe(name="pthread", sym="pthread_join", fn_name="probe_join", pid=int(pid))
