491d2c7b08e0cd1062a795e00b30aa9f163f7266
linouya <linouyacn@gmail.com>
2020-08-19 15:48:38 +0800

    update

--- src/main/locktime.c
+++ src/main/locktime.c
@@ -134,7 +134,7 @@ int probe_join(struct pt_regs *ctx){
     struct time_k unit = {};
     unit.timestamp = now;
     unit.tid = bpf_get_current_pid_tgid();
-    unit.runtime_id = PT_REGS_PARM1(ctx)
+    unit.runtime_id = PT_REGS_PARM1(ctx);
     __builtin_memcpy(&unit.type, type, sizeof(unit.type));
     times.increment(unit);
     return 0;
