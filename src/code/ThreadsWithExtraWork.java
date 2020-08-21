01264428801134284c0b4e158afe7db38c4aed8b
linouya <linouyacn@gmail.com>
2020-08-21 16:16:15 +0800

    update

---
+++ src/code/ThreadsWithExtraWork.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ThreadsWithExtraWork {
+
+    public static volatile Long val = 0L;
+
+    public static void main(String[] args) throws InterruptedException {
+        int times = 1;
+        while(times > 0) {
+            times--;
+            TestThread testThread = new TestThread();
+            Thread thread1 = new Thread(testThread);
+            Thread thread2 = new Thread(testThread);
+            thread1.start();
+            thread2.start();
+
+            thread1.join();
+            thread2.join();
+            System.out.println(val);
+        }
+    }
+
+    public static class TestThread implements Runnable{
+        @Override
+        public void run() {
+            synchronized (this) {
+                System.out.println("now tid: " + Thread.currentThread().getId());
+                for (int i = 0; i < 500000000; i++) {
+                    val++;
+                }
+            }
+            for (int i = 0; i < 500000000; i++) {
+                val++;
+            }
+        }
+    }
+}
+
