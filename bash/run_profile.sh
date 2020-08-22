e6a1dbf859a3782b2581b1fc95460f97435e79a7
linouya <linouyacn@gmail.com>
2020-08-22 23:49:33 +0800

    update

--- bash/run_profile.sh
+++ bash/run_profile.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-# dir
-rm -rf out
-mkdir out
+# sh run_java.sh 60
+
 time=$1
-name="avrora"
-# -s large -n 5
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 2 $name &
-sleep 1
+burn_path="../resources"
+out_path="../src/output"
+
+rm -rf $out_path
+mkdir $out_path
+mkdir $out_path/stack
+
+# Modify java program: Replace 'SingleFun' in java and java_name
+# java program shows in ../java/
+cd ../src/code
+name="Threads" #ThreadsWithLock
+javac $name.java
+# -Xmx2m -XX:ReservedCodeCacheSize=16M
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints $name &
+#sleep 1
+cd ..
 
 pid=$(pgrep -f "$name")
-echo "program pid: "  $pid
+echo "program pid: " $pid
 
+# jstack
+output=`jstack $pid > output/out_stack.log`
+# perf map
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
-output=`~/bcc/tools/profile.py -adf -p $pid $time > out/out.profile`
-output=`~/FlameGraph/flamegraph.pl < out/out.profile > out/out.svg`
 
 
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 2 avrora
-pgrep -f avrora
-
-sh ~/perf-map-agent/bin/create-java-perf-map.sh 25206 "unfoldall,dottedclass"
-~/bcc/tools/profile.py -adf -p 25206 15 > out.profile
+# eBPF
+chmod 777 ../resources/learn/lockstat.py
+output=`../resources/learn/lockstat.py $pid`
