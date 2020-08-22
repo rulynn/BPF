288924a51b379501bc4e9458a70a983e25d7563b
linouya <linouyacn@gmail.com>
2020-08-22 13:31:27 +0800

    update

--- src/main/locktime.py
+++ src/main/locktime.py
@@ -44,7 +44,7 @@ if not language:
     language = utils.detect_language(languages, args.pid)
 
 usdt = USDT(pid=args.pid)
-# usdt.enable_probe_or_bail("pthread_start", "trace_pthread")
+usdt.enable_probe_or_bail("pthread_start", "trace_pthread")
 
 # load BPF program
 if language == "java":
@@ -63,7 +63,7 @@ bpf.attach_uprobe(name="pthread", sym="pthread_mutex_unlock", fn_name="probe_mut
 bpf.attach_uprobe(name="pthread", sym="pthread_create", fn_name="probe_create", pid=args.pid)
 # bpf.attach_uprobe(name="pthread", sym="pthread_exit", fn_name="probe_exit", pid=int(pid))
 # bpf.attach_uprobe(name="pthread", sym="pthread_mutex_trylock", fn_name="probe_mutex_trylock", pid=int(pid))
-bpf.attach_uprobe(name="pthread", sym="pthread_join", fn_name="probe_join", pid=args.pid)
+#bpf.attach_uprobe(name="pthread", sym="pthread_join", fn_name="probe_join", pid=args.pid)
 # bpf.attach_uprobe(name="pthread", sym="pthread_cancel", fn_name="probe_cancel", pid=int(pid))
 # bpf.attach_uprobe(name="pthread", sym="pthread_barrier_init", fn_name="probe_barrier_init", pid=int(pid))
 
