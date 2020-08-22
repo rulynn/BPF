a912d597e6a90a999cb1f58aa26f8a729c2c001f
linouya <linouyacn@gmail.com>
2020-08-22 21:31:17 +0800

    update

--- src/code/Single.java
+++ src/code/Single.java
@@ -21,7 +21,7 @@ public class Single {
 //        }
         long val = 0;
         for (int i = 0; i < 1000000; i++) {
-            for (int j = 0; j < 100000; j++) {
+            for (int j = 0; j < 1000000; j++) {
                 val++;
             }
         }
