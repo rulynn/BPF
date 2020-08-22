01497535d06d4ac821c4289cb869ca62d5ae7560
linouya <linouyacn@gmail.com>
2020-08-22 21:23:27 +0800

    update

--- src/code/Single.java
+++ src/code/Single.java
@@ -7,19 +7,22 @@ public class Single {
 
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        long end = 0;
-        ArrayList<Integer> integers = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 25000; i++) {
-            integers.add(random.nextInt(100000));
+//        ArrayList<Integer> integers = new ArrayList<>();
+//        Random random = new Random();
+//        for (int i = 0; i < 25000; i++) {
+//            integers.add(random.nextInt(100000));
+//        }
+//        for (int i = 0; i < 10000; i++) {
+//            for (Integer integer : integers) {
+//                integer = integer + random.nextInt(100);
+//            }
+//
+//        }
+        long val = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            val++;
         }
-        for (int i = 0; i < 10000; i++) {
-            for (Integer integer : integers) {
-                integer = integer + random.nextInt(100);
-            }
-
-        }
-        end = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
         System.out.println(end - start);
     }
 }
