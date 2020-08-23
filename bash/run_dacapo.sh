ff9138201ea5637e128949a1aee853682547d57c
linouya <linouyacn@gmail.com>
2020-08-23 21:55:58 +0800

    update

--- bash/run_dacapo.sh
+++ bash/run_dacapo.sh
@@ -19,10 +19,10 @@ java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVM
 pid=$(pgrep -f "$name")
 echo "program pid: " $pid
 
+# perf map
+sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass" &
 # jstack
 output=`jstack $pid > output/out_stack.log`
-# perf map
-output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
 # eBPF
 chmod 777 main/locktime.py
 output=`main/locktime.py -l java -p $pid -t $time> output/out.log`
