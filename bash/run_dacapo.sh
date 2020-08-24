f811f7fbc3da420d4e469024272deba35641ff90
linouya <linouyacn@gmail.com>
2020-08-24 14:30:28 +0800

    update

--- bash/run_dacapo.sh
+++ bash/run_dacapo.sh
@@ -14,17 +14,22 @@ mkdir $out_path/stack
 cd ../src
 
 # Dacapo -s large -n 5 -Xmx1024m -XX:ReservedCodeCacheSize=64M -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -jar ~/dacapo.jar -n 5 $name &
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ~/dacapo.jar -n 5 $name &
 
 pid=$(pgrep -f "$name")
 echo "program pid: " $pid
+echo "now timeStamp: " $[$(date +%s%N)/1000000]
+
 # jstack
 output=`jstack $pid > output/out_stack.log`
+echo "jstack timeStamp: " $[$(date +%s%N)/1000000]
 # perf map
 output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
+echo "perf timeStamp: " $[$(date +%s%N)/1000000]
 # eBPF
 chmod 777 main/locktime.py
 output=`main/locktime.py -l java -p $pid -t $time> output/out.log`
+echo "eBPF timeStamp: " $[$(date +%s%N)/1000000]
 
 # burn: convert data to json
 chmod 777 $burn_path/burn
