9bca4c7f4109bc70794967f5bb6e06f897198114
linouya <linouyacn@gmail.com>
2020-08-26 22:19:16 +0800

    update

--- src/code/ThreadsWithLock2.java
+++ src/code/ThreadsWithLock2.java
@@ -4,7 +4,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ThreadsWithLock2 {
 
     public static volatile Long val = 0L;
-    public static volatile Integer id = 3;
+//    public static volatile Integer id = 3;
 
     private static Lock lock = new ReentrantLock();
 
@@ -13,21 +13,28 @@ public class ThreadsWithLock2 {
         int times = 1;
         while(times > 0) {
             times--;
-            TestThread testThread = new TestThread();
-            Thread thread1 = new Thread(testThread);
-            Thread thread2 = new Thread(testThread);
+            TestThread testThread1 = new TestThread(6);
+            TestThread testThread2 = new TestThread(3);
+            Thread thread1 = new Thread(testThread1);
+            Thread thread2 = new Thread(testThread2);
             thread1.start();
             thread2.start();
         }
     }
 
     public static class TestThread implements Runnable{
+
+        public volatile Integer id;
+
+        public TestThread(int id){
+            this.id = id;
+        }
+
         @Override
         public void run() {
             lock.lock();
             long start = System.currentTimeMillis();
             System.out.println("now tid: " + Thread.currentThread().getId() + " ::: start time: " + start);
-            System.out.println(id);
             for (int i = 0; i < id * 100000000; i++) {
                 val++;
                 if (i % 100000000 == 0) {
@@ -35,7 +42,6 @@ public class ThreadsWithLock2 {
                     System.out.println("now tid: " + Thread.currentThread().getId() + " ::: time: " + end);
                 }
             }
-            id *= 2;
             long end = System.currentTimeMillis();
             System.out.println("now tid: " + Thread.currentThread().getId() + " ::: finish time: " + end);
             lock.unlock();
