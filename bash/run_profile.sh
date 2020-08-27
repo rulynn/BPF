9607680578400646bb5a0ceeb264603b94da348a
linouya <linouyacn@gmail.com>
2020-08-27 16:15:41 +0800

    update

--- bash/run_profile.sh
+++ bash/run_profile.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# sh run_java.sh 60
-
 time=$1
+name="eclipse"
 burn_path="../resources"
 out_path="../src/output"
+
 
 rm -rf $out_path
 mkdir $out_path
 mkdir $out_path/stack
+cd ../src
 
-# Modify java program: Replace 'SingleFun' in java and java_name
-# java program shows in ../java/
-cd ../src/code
-name="Threads" #ThreadsWithLock
-javac $name.java
-# -Xmx2m -XX:ReservedCodeCacheSize=16M
-java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints $name &
-#sleep 1
-cd ..
+# Dacapo -s large -n 5 -Xmx1024m -XX:ReservedCodeCacheSize=64M -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints
+java -XX:+ExtendedDTraceProbes -XX:+PreserveFramePointer -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ~/dacapo.jar -n 5 $name &
 
 pid=$(pgrep -f "$name")
 echo "program pid: " $pid
 
-# jstack
-output=`jstack $pid > output/out_stack.log`
 # perf map
-output=`sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass"`
-
+sh ~/perf-map-agent/bin/create-java-perf-map.sh $pid "unfoldall,dottedclass" &
 
 # eBPF
-chmod 777 ../resources/learn/lockstat.py
-output=`../resources/learn/lockstat.py $pid`
+chmod 777 ../resources/profile.py
+output=`../resources/profile -f $time $pid > $out_path/stack/all.log`
+
+$burn_path/burn convert --type=folded $out_path/stack/all.log > $out_path/stack/all.log.json
+
+
